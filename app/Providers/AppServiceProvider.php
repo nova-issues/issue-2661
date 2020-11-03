3dfebb5e939fec4584a1faf1c75605624826ef58
Mior Muhammad Zaki <crynobone@gmail.com>
2020-11-03 08:26:50 +0800

    Add reproducing code and fixes for laravel/nova-issues#2661
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(
+            \Laravel\Vapor\Contracts\SignedStorageUrlController::class,
+            \App\Http\Controllers\SignedStorageUrlController::class
+        );
     }
 }
